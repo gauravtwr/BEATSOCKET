1efa43db419bc54256d1e3a2d9c17fe692c9011f
gauravtwr <gauravtiwari20021995@gmail.com>
2021-06-14 12:46:57 +0530

    setup db connection

--- server.js
+++ server.js
@@ -34,45 +34,22 @@ app.listen(process.env.PORT, () => {
   console.log("Express server started at port: " + process.env.PORT);
 });
 
-// mongoose.connect(
-//   process.env.MONGODB_URI,
-//   {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//     useCreateIndex: true,
-//     useFindAndModify: false,
-//   },
-//   (err) => {
-//     if (!err) {
-//       console.log("Database connected");
-//       // const conn = mongoose.createConnection(process.env.MONGODB_URI);
-//       // let gfs;
-//       // conn.once("open", () => {
-//       //   gfs = Grid(conn.db, mongoose.mongo);
-//       //   gfs.collection("uploads");
-//       //   console.log("Database connected");
-//       // });
-//     } else console.log(err);
-//   }
-// );
+mongoose.connect(
+  process.env.MONGODB_URI,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  },
+  (err) => {
+    if (!err) {
+      console.log("Database connected");
+      
+    } else console.log(err);
+  }
+);
 
-// const storage = new GridFsStorage({
-//   url: process.env.MONGODB_URI,
-//   file: (req, file) => {
-//     return new Promise((resolve, reject) => {
-//       crypto.randomBytes(16, (err, buf) => {
-//         if (err) return reject(err);
-//         const filename = buf.toString("hex") + path.extname(file.originalname);
-//         const fileInfo = {
-//           filename: filename,
-//           bucketName: "uploads",
-//         };
-//         resolve(fileInfo);
-//       });
-//     });
-//   },
-// });
-// const upload = multer({ storage });
 
 app.use("/", homeController);
 app.use("/user", userController);
